e0e2ea9fffcc07f9901998dc1a807e8b8eeca6e7
Lucas Winicius <lucasclucas7@gmail.com>
2025-10-28 20:06:17 -0300

    aulas

---
+++ 9. Outubro/28.10/pazin/ex 5/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <locale.h>
+#define LINHA 3
+#define COLUNA 3
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+    int matriz[LINHA][COLUNA];
+    printf("Digite os elementos da matriz:\n");
+    for (int i = 0; i < LINHA; i++)
+    {
+        for (int j = 0; j < COLUNA; j++)
+        {
+            if ((j < i) == 0)
+            {
+                printf("\nPosicao [%d][%d]: ", i, j);
+                scanf("%d", &matriz[i][j]);
+            }
+            else
+            {
+                printf("\nPosicao [%d][%d]: 0", i, j);
+                matriz[i][j] = 0;
+            }
+        }
+    }
+
+    printf("\nMatriz formatada:\n");
+    for (int i = 0; i < LINHA; i++)
+    {
+        for (int j = 0; j < COLUNA; j++)
+        {
+            printf("%d\t", matriz[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
